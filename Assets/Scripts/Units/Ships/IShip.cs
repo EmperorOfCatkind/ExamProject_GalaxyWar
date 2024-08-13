06255797d6b14c4019da225e36c566f2177c752e
EmperorOfCatkind <kirillaleksandrov84@gmail.com>
2024-08-13 18:17:59 +0200

    Repository replacement

---
+++ Assets/Scripts/Units/Ships/IShip.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public interface IShip
+{
+    string Name {get; set;}
+    GridPosition gridPosition {get; set;}
+    
+}
---
+++ Assets/Scripts/Units/UnitController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+    [SerializeField] private GameObject shipPrefab;
+    [SerializeField] private LayerMask shipLayerMask;
+    private Vector3 shipYOffset;
+    private IShip selectedShip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shipYOffset = new Vector3(0, 3, 0);
+
+        SpawnShip(shipPrefab, new GridPosition(0, 0));  //debug purposes
+        SpawnShip(shipPrefab, new GridPosition(0, 0));  //debug purposes
+        SpawnShip(shipPrefab, new GridPosition(0, 0));  //debug purposes
+        SpawnShip(shipPrefab, new GridPosition(0, 0));  //debug purposes
+        SpawnShip(shipPrefab, new GridPosition(0, 0));  //debug purposes
+        SpawnShip(shipPrefab, new GridPosition(0, 0));  //debug purposes
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(TrySelectShip()) return;
+        }
+    }
+
+    public void SpawnShip(GameObject prefab, GridPosition gridPosition)
+    {
+        MapGridViewSingle hexToSpawn = MapController.Instance.GetMapGridViewSingle(gridPosition);
+
+        SpaceWaypoint availableWaypoint = hexToSpawn.GetAvailableWaypoint();
+
+        if(availableWaypoint == null)
+        {
+            return;
+        }
+
+        Instantiate(prefab, availableWaypoint.transform.position + shipYOffset, Quaternion.identity);
+        availableWaypoint.SetHasUnit(true);
+    }
+
+    public bool TrySelectShip()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        bool OnShip = Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, shipLayerMask);
+
+        if(OnShip)
+        {
+            /*if(raycastHit.transform.TryGetComponent<IShip>(out IShip ship))
+            {
+                SetSelectedShip(ship);
+                Debug.Log(selectedShip.Name);
+                return true;
+            }*/
+            GameObject selectedObject = raycastHit.collider.gameObject;
+        }
+
+        return false;
+    }
+
+    public void SetSelectedShip(IShip ship)
+    {
+        selectedShip = ship;
+    }
+}
