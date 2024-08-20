c25646a59dc67a3ed08b2868272290480428adf9
EmperorOfCatkind <kirillaleksandrov84@gmail.com>
2024-08-20 20:36:26 +0200

    Added GameMaster

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -2,30 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test_GameMaster : MonoBehaviour
+public class GameMaster : MonoBehaviour
 {
+    public static GameMaster Instance;
+
     private MapController mapController;
-    private UnitController unitController;
     private PlayerTurnController playerTurnController;
+    private UnitController unitController;
+
+    void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+        
+    }
     // Start is called before the first frame update
     void Start()
     {
         mapController = MapController.Instance;
-        unitController = UnitController.Instance;
         playerTurnController = PlayerTurnController.Instance;
+        unitController = UnitController.Instance;
 
         playerTurnController.InitializePlayers();
         playerTurnController.turnStateMachine.OnPhaseChanged += playerTurnController.OnPhaseChanged;
         playerTurnController.InitializePlayersFleets();
         playerTurnController.InitializeFirstPlayer();
 
-        /*unitController.SpawnShip(unitController.shipPrefab, new GridPosition(0,1), PlayerType.PlayerOne); //debug purposes
         unitController.SpawnShip(unitController.shipPrefab, new GridPosition(0,1), PlayerType.PlayerOne); //debug purposes
-        unitController.SpawnShip(unitController.shipPrefab, new GridPosition(0,1), PlayerType.PlayerOne); //debug purposes*/
+        unitController.SpawnShip(unitController.shipPrefab, new GridPosition(0,1), PlayerType.PlayerOne); //debug purposes
+        unitController.SpawnShip(unitController.shipPrefab, new GridPosition(0,1), PlayerType.PlayerOne); //debug purposes
 
-        /*unitController.SpawnShip(unitController.shipPrefab, new GridPosition(0,1), PlayerType.PlayerTwo); //debug purposes
         unitController.SpawnShip(unitController.shipPrefab, new GridPosition(0,1), PlayerType.PlayerTwo); //debug purposes
-        unitController.SpawnShip(unitController.shipPrefab, new GridPosition(0,1), PlayerType.PlayerTwo); //debug purposes*/
+        unitController.SpawnShip(unitController.shipPrefab, new GridPosition(0,1), PlayerType.PlayerTwo); //debug purposes
+        unitController.SpawnShip(unitController.shipPrefab, new GridPosition(0,1), PlayerType.PlayerTwo); //debug purposes
     }
 
     // Update is called once per frame
